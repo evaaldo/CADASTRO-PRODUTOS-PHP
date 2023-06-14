a1c1c69688a28ee939b6317b97baf2654fec49b9
Evaldo <evaaldormj@gmail.com>
2023-06-14 18:47:11 -0300

    Feat: CSS básico

--- index.php
+++ index.php
@@ -7,6 +7,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Acessar treinos</title>
+
+    <link rel="stylesheet" href="/style/style.css">
+    <link rel="stylesheet" href="/style/login.css">
 </head>
 <body>
     <section class="box-login">
@@ -16,10 +19,10 @@
         <div class="box-credenciais">
             <form class="box-form" method="post">
                 <label for="nome">Nome</label>
-                <input type="text" name="nome" id="nome" placeholder="Nome.." required>
+                <input class="box-form-input" type="text" name="nome" id="nome" placeholder="Nome.." required>
                 <label for="senha">Senha</label>
-                <input type="text" name="senha" id="senha" placeholder="Senha.." required>
-                <input type="submit" value="Entrar" name="login">
+                <input class="box-form-input" type="password" name="senha" id="senha" placeholder="Senha.." required>
+                <input class="btn-login" type="submit" value="Entrar" name="login">
             </form>
             <a class="signUp" href="cadastro.php">Cadastrar-se</a>
             <a class="github" href="https://github.com/evaaldo">Conheça mais sobre o nosso desenvolvedor!</a>
@@ -27,24 +30,24 @@
 
         <?php
 
-        include("conexao.php");
+        // include("conexao.php");
 
-        if(isset($_POST['login'])) {
-            $nome = $_POST['nome'];
-            $senha = $_POST['senha'];
+        // if(isset($_POST['login'])) {
+        //     $nome = $_POST['nome'];
+        //     $senha = $_POST['senha'];
 
-            $sql = "SELECT * FROM tb_usuario WHERE nome = '$nome' AND senha = '$senha'";
-            $sql_query = $conexao->query($sql) or exit($conexao->error);
+        //     $sql = "SELECT * FROM tb_usuario WHERE nome = '$nome' AND senha = '$senha'";
+        //     $sql_query = $conexao->query($sql) or exit($conexao->error);
 
-            $quantidade = $sql_query->num_rows;
+        //     $quantidade = $sql_query->num_rows;
 
-            if($quantidade == 1) {
-                $_SESSION['login'] = $_POST['nome'];
-                header("Location: home.php");
-            } else {
-                header("Location: erro.php");
-            }
-        }
+        //     if($quantidade == 1) {
+        //         $_SESSION['login'] = $_POST['nome'];
+        //         header("Location: home.php");
+        //     } else {
+        //         header("Location: erro.php");
+        //     }
+        // }
         ?>
 
     </section>
